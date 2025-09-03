7ed2be7881223044c7434fa9f0794b89573d9ac1
Sachinkr195 <151429353+Sachinkr195@users.noreply.github.com>
2025-09-03 23:41:22 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import Navbar from './components/Navbar'
-import Hero from './components/hero'
+import Hero from './components/Hero'
 import Card from './components/Card'
 
 import './App.css'
